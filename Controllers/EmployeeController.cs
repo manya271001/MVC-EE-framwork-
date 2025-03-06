33ce357dd4c5b1173e9ea48634caa9c261795679
Manya Sharma <manya271001@gmail.com>
2025-03-06 16:38:16 +0530

    MVC demo

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult EmpDelete()
+        {
+            return View();
+        }
+        public IActionResult EmpList()
+        {
+            List<Employee> obj = new List<Employee>
+  {
+      new Employee {Id=101,firstName="Manya",lastName="Sharma",city="Delhi"},
+      new Employee {Id=102,firstName="Shradhha",lastName="Balapurkur",city="Delhi"},
+      new Employee {Id=103,firstName="Anshita",lastName="Dilarj",city="Delhi" },
+      new Employee {Id=104,firstName="Hrishabh",lastName="Dubey",city="Delhi" },
+      new Employee {Id=105,firstName="Akshat",lastName="Sharma",city="Delhi" }
+  };
+            return View(obj);
+        }
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string city { get; set; }
+
+    }
+}
